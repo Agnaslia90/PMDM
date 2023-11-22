825a0467a062f77eae198c9fbf461afeacafc52c
Natalia <natalvromfoc@gmail.com>
2023-11-22 18:41:38 +0100

    feat: clean code

--- app/src/main/java/com/example/pmdm_t2/MainActivity.java
+++ app/src/main/java/com/example/pmdm_t2/MainActivity.java
@@ -1,27 +1,27 @@
 package com.example.pmdm_t2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.net.NetworkCapabilities;
-import android.net.Uri;
-import android.view.View;
-import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebSettings;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
-import android.content.SharedPreferences;
+import android.net.NetworkCapabilities;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +31,63 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initializeViews() {
-        Button btPulsandoConexion = (Button) findViewById(R.id.btConexion);
-        btPulsandoConexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (hayConectividad(getApplicationContext())) {
-                    Toast.makeText(getApplicationContext(), "Hay conexión a internet", Toast.LENGTH_SHORT).show();
+        setUpBtnConnectionClick();
+        setUpBtnSensorClick();
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "No hay conexión a internet", Toast.LENGTH_SHORT).show();
+        text = findViewById(R.id.txtText);
+
+        setUpBtnSaveClick();
+        setUpBtnLoadClick();
+        setUpWebView();
+    }
+
+    private void setUpWebView() {
+        WebView myWebView = (WebView) findViewById(R.id.WebView);
+        myWebView.clearCache(true);
+        myWebView.clearHistory();
+        myWebView.loadUrl("https://www.hbomax.com/es/es/series/urn:hbo:series:GXdbR_gOXWJuAuwEAACVH");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        Uri webpage = Uri.parse("https://www.hbomax.com/es/es/series/urn:hbo:series:GXdbR_gOXWJuAuwEAACVH");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void setUpBtnLoadClick() {
+        Button btPulsandoCargar = findViewById(R.id.btCargar);
+        btPulsandoCargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View s) {
+                SharedPreferences sharePref = getSharedPreferences("Datos", Context.MODE_PRIVATE);
+                String TextoCompleto = sharePref.getString("datos", "");
+                text.setText(TextoCompleto);
+                Toast.makeText(getApplicationContext(), "Datos cargados", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void setUpBtnSaveClick() {
+        Button btPulsandoGuardar = (Button) findViewById(R.id.btGuardar);
+        btPulsandoGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View c) {
+                if (c.getId() == R.id.btGuardar) {
+                    String TextoCompleto = text.getText().toString();
+                    SharedPreferences sharePref = getSharedPreferences("Datos", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor edit = sharePref.edit();
+                    edit.putString("datos", TextoCompleto);
+                    edit.apply();
+
+                    Toast.makeText(getApplicationContext(), "Datos guardados", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
 
+    private void setUpBtnSensorClick() {
         Button btPulsandoSensor = (Button) findViewById(R.id.btSensor);
         btPulsandoSensor.setOnClickListener(new View.OnClickListener() {
 
@@ -57,55 +101,21 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
 
-        Button btPulsandoCargar = (Button) findViewById(R.id.btCargar);
-        Button btPulsandoGuardar = (Button) findViewById(R.id.btGuardar);
-
-        EditText Texto = findViewById(R.id.txtText);
-
-        btPulsandoGuardar.setOnClickListener(new View.OnClickListener() {
+    private void setUpBtnConnectionClick() {
+        Button btPulsandoConexion = (Button) findViewById(R.id.btConexion);
+        btPulsandoConexion.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View c) {
-                if (c.getId() == R.id.btGuardar) {
-                    String TextoCompleto = Texto.getText().toString();
+            public void onClick(View v) {
+                if (hayConectividad(getApplicationContext())) {
+                    Toast.makeText(getApplicationContext(), "Hay conexión a internet", Toast.LENGTH_SHORT).show();
 
-                    SharedPreferences sharePref = getSharedPreferences("Datos", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor edit = sharePref.edit();
-                    edit.putString("datos", TextoCompleto);
-                    edit.apply();
-
-                    Toast.makeText(getApplicationContext(), "Datos guardados", Toast.LENGTH_LONG).show();
-
+                } else {
+                    Toast.makeText(getApplicationContext(), "No hay conexión a internet", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        btPulsandoCargar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View s) {
-                SharedPreferences sharePref = getSharedPreferences("Datos", Context.MODE_PRIVATE);
-                String TextoCompleto = sharePref.getString("datos", "");
-
-                Texto.setText(TextoCompleto);
-
-                Toast.makeText(getApplicationContext(), "Datos cargados", Toast.LENGTH_LONG).show();
-
-            }
-
-        });
-
-        WebView myWebView = (WebView) findViewById(R.id.WebView);
-        myWebView.clearCache(true);
-        myWebView.clearHistory();
-        myWebView.loadUrl("https://www.hbomax.com/es/es/series/urn:hbo:series:GXdbR_gOXWJuAuwEAACVH");
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        Uri webpage = Uri.parse("https://www.hbomax.com/es/es/series/urn:hbo:series:GXdbR_gOXWJuAuwEAACVH");
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
     }
 
     public boolean haySensor() {
